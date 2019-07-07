eb990ee5d2166615eb80f23193fc23fc5e974a8b
elias-garcia <eliasgarciadev@gmail.com>
2019-07-07 17:11:41 +0200

    Financing widget removed unused code

--- financing-widget/src/components/Financing/FinancingHeader/FinancingHeader.js
+++ financing-widget/src/components/Financing/FinancingHeader/FinancingHeader.js
@@ -4,7 +4,7 @@ import './FinancingHeader.css';
 function FinancingHeader() {
   return (
     <div className="FinancingHeader">
-      <p className="FinancingHeader__text">Págalo en</p>
+      <p>Págalo en</p>
       <button className="FinancingHeader__button" type="button">Más info</button>
     </div>
   );
